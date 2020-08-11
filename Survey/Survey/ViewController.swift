e2b245e06de16fd074e4197b13598e5ba22dbb31
Menesidis <menesidis@gmail.com>
2020-08-11 14:58:35 +0300

    Add initial Survey app

---
+++ Survey/Survey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Survey
+//
+//  Created by Menesidis on 11/8/20.
+//  Copyright © 2020 Survey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
