99bc85ea5e50db7cb34b3b44a7eb9627fcd1c9c1
Menesidis <menesidis@gmail.com>
2020-08-11 16:15:31 +0300

    Setup layout for landing viewcontroller

--- Survey/Survey/Views/LandingViewController.swift
+++ Survey/Survey/Views/LandingViewController.swift
@@ -10,21 +10,27 @@ import UIKit
 
 class LandingViewController: UIViewController {
 
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupView()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    private func setupView() {
+        descriptionLabel.textColor = .black
+        descriptionLabel.font = UIFont.preferredFont(forTextStyle: .title1)
+        descriptionLabel.adjustsFontForContentSizeCategory = true
+        descriptionLabel.text = "Welcome to survey app"
+        descriptionLabel.numberOfLines = 0
+        
+        startButton.setTitle("Start survey", for: .normal)
+        startButton.backgroundColor = .white
+        startButton.setTitleColor(.blue, for: .normal)
+        startButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .title2)
+        startButton.titleLabel?.adjustsFontForContentSizeCategory = true
+        startButton.rounded(cornerRadius: 15.0, borderColor: .blue, borderWidth: 0.5)
     }
-    */
-
 }
