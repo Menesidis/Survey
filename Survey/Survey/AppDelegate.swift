0c6c6e7608dc38c1863b9fc7d55e47da07306bb0
Menesidis <menesidis@gmail.com>
2020-08-11 16:42:44 +0300

    Set app coordinator as a strong reference

--- Survey/Survey/AppDelegate.swift
+++ Survey/Survey/AppDelegate.swift
@@ -12,13 +12,14 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var appCoordinator: AppCoordinator?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         let navigationController = UINavigationController()
-        let appCoordinator = AppCoordinator(navigationController: navigationController)
-        appCoordinator.start()
+        appCoordinator = AppCoordinator(navigationController: navigationController)
+        appCoordinator?.start()
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = navigationController
