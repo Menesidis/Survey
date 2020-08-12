245e35e0c7f3c3b1e9e620fd11780cf856997831
Menesidis <menesidis@gmail.com>
2020-08-12 21:22:38 +0300

    Add question target to handle both APIs

---
+++ Survey/Survey/Network/Targets/QuestionTarget.swift
@@ -0,0 +1,61 @@
+//
+//  QuestionTarget.swift
+//  Survey
+//
+//  Created by Menesidis on 12/8/20.
+//  Copyright © 2020 Survey. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+enum QuestionTarget {
+    case questions
+    case submit(parameters: [String: Any])
+}
+
+extension QuestionTarget: TargetType {
+
+    var baseURL: URL {
+        return URL(string: "https://powerful-peak-54206.herokuapp.com/")!
+    }
+
+    var path: String {
+        switch self {
+        case .questions:
+            return "questions"
+        case .submit:
+            return "question/submit"
+        }
+    }
+
+    var method: Moya.Method {
+        switch self {
+        case .questions:
+            return .get
+        case .submit:
+            return .post
+        }
+    }
+
+    var task: Task {
+        switch self {
+        case .questions:
+            return .requestPlain
+        case .submit(parameters: let parameters):
+            return .requestParameters(parameters: parameters, encoding: URLEncoding.default)
+        }
+    }
+
+    var headers: [String: String]? {
+        let defaultHeaders = [
+            "Content-type": "application/json"
+        ]
+
+        return defaultHeaders
+    }
+
+    var sampleData: Data {
+        return Data()
+    }
+}
