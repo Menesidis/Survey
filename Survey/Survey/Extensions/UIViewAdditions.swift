082d49529e33e81c0f0e1a73252c48a4c4d3bf9f
Menesidis <menesidis@gmail.com>
2020-08-13 20:45:30 +0300

    Add extension methods to animate UIView (fadeIn - fadeOut)

--- Survey/Survey/Extensions/UIViewAdditions.swift
+++ Survey/Survey/Extensions/UIViewAdditions.swift
@@ -27,4 +27,26 @@ extension UIView {
             layer.borderWidth = borderWidth
         }
     }
+    
+    func fadeOut(duration: TimeInterval = 0.5,
+                 delay: TimeInterval = 0.0,
+                 completion: @escaping (Bool) -> Void = {(finished: Bool) -> Void in }) {
+      UIView.animate(withDuration: duration,
+                     delay: delay,
+                     options: UIView.AnimationOptions.curveEaseIn,
+                     animations: {
+        self.alpha = 0.0
+      }, completion: completion)
+    }
+    
+    func fadeIn(duration: TimeInterval = 0.5,
+                delay: TimeInterval = 0.0,
+                completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in }) {
+      UIView.animate(withDuration: duration,
+                     delay: delay,
+                     options: UIView.AnimationOptions.curveEaseOut,
+                     animations: {
+        self.alpha = 1.0
+      }, completion: completion)
+    }
 }
