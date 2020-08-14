82ffdca40c040d5d3c9abc82f0261573369863e0
Menesidis <menesidis@gmail.com>
2020-08-14 18:46:43 +0300

    Refactor, using DI with default values

--- Survey/Survey/Interactor/QuestionsInteractor.swift
+++ Survey/Survey/Interactor/QuestionsInteractor.swift
@@ -10,7 +10,7 @@ import Foundation
 import RxSwift
 
 protocol QuestionsInteractorType: InteractorType {
-    func load(page: Int, answer: String?, notificationState: NotificationState) -> Observable<QuestionDetails>
+    func load(answer: String?, notificationState: NotificationState) -> Observable<QuestionDetails>
     func next() -> Observable<QuestionDetails>
     func previous() -> Observable<QuestionDetails>
     func updateAnswer(answer: String) -> Observable<QuestionDetails>
@@ -19,30 +19,33 @@ protocol QuestionsInteractorType: InteractorType {
 
 extension QuestionsInteractorType {
     func load() -> Observable<QuestionDetails> {
-        return load(page: 1, answer: nil, notificationState: .none)
+        return load(answer: nil, notificationState: .none)
     }
 }
 
 class QuestionsInteractor: QuestionsInteractorType {
     private let repository: QuestionsRepositoryType
-    private var currentPage: Int = 0
-    private var totalPages: Int = 0
-    private var currentAnswer: String?
+    private var currentPage: Int
+    private var totalPages: Int
+    private var currentAnswer: String
     private var submittedQuestions = [Int: String]() // Current page & answer text accordingly
     
     deinit {
         print("♻️🚮 \(#file): \(#function)")
     }
     
-    init(questionsRepository: QuestionsRepositoryType) {
+    init(questionsRepository: QuestionsRepositoryType, currentPage: Int = 1, totalPages: Int = 1, currentAnswer: String = "") {
         print("♻️🆕 \(#file): \(#function)")
         self.repository = questionsRepository
+        self.currentPage = currentPage
+        self.totalPages = totalPages
+        self.currentAnswer = currentAnswer
     }
     
     func next() -> Observable<QuestionDetails> {
         if currentPage < totalPages {
             currentPage += 1
-            return load(page: currentPage)
+            return load()
         } else {
             return Observable.empty()
         }
@@ -51,34 +54,31 @@ class QuestionsInteractor: QuestionsInteractorType {
     func previous() -> Observable<QuestionDetails> {
         if currentPage > 1 && currentPage <= totalPages {
             currentPage -= 1
-            return load(page: currentPage)
+            return load()
         } else {
             return Observable.empty()
         }
     }
     
     func updateAnswer(answer: String) -> Observable<QuestionDetails> {
-         self.currentAnswer = answer
-        return load(page: currentPage, answer: answer)
+        self.currentAnswer = answer
+        return load(answer: answer)
     }
     
     func submit() -> Observable<QuestionDetails> {
-        guard let answer = currentAnswer else { return Observable.empty() }
-        let request = AnswerRequest(page: currentPage, answer: answer)
+        let request = AnswerRequest(page: currentPage, answer: currentAnswer)
                 
         return repository.submit(request: request)
             .flatMap { [unowned self] isSucessful -> Observable<QuestionDetails> in
                 if isSucessful {
-                    self.submittedQuestions[self.currentPage] = answer
+                    self.submittedQuestions[self.currentPage] = self.currentAnswer
                 }
                 let notificationState: NotificationState = isSucessful ? .sucessful : .failed
-                return self.load(page: self.currentPage, answer: answer, notificationState: notificationState)
+                return self.load(answer: self.currentAnswer, notificationState: notificationState)
         }
     }
     
-    //TODO: Check multiple API calls!
-    //TODO: Update mapping logic
-    func load(page: Int, answer: String? = nil, notificationState: NotificationState = .none) -> Observable<QuestionDetails> {
+    func load(answer: String? = nil, notificationState: NotificationState = .none) -> Observable<QuestionDetails> {
         let totalPagesObservable = repository.questions().map { $0.count }
         let questionsObservable =  repository.questions()
         
@@ -87,11 +87,10 @@ class QuestionsInteractor: QuestionsInteractorType {
                 
                 // Set variables
                 self.totalPages = totalPages
-                self.currentPage = page
                 
                 let buttonType: ButtonType
                 let answeredQuestion: String
-                if let answered = self.submittedQuestions[page] {
+                if let answered = self.submittedQuestions[self.currentPage] {
                     buttonType = .submitted
                     answeredQuestion = answered
                 } else if let answer = answer, answer.isValid {
@@ -103,13 +102,13 @@ class QuestionsInteractor: QuestionsInteractorType {
                 }
                 
                 let questionDetails = questions
-                    .filter { $0.identifier == page }
+                    .filter { $0.identifier == self.currentPage }
                     .first
                     .map {
-                        QuestionDetails(title: "Question \(page)/\(totalPages)",
+                        QuestionDetails(title: "Question \(self.currentPage)/\(totalPages)",
                             name: $0.name,
-                            previousEnabled: self.previousEnabled(page: page, totalPages: totalPages),
-                            nextEnabled: self.nextEnabled(page: page, totalPages: totalPages),
+                            previousEnabled: self.previousEnabled(page: self.currentPage, totalPages: totalPages),
+                            nextEnabled: self.nextEnabled(page: self.currentPage, totalPages: totalPages),
                             submittedQuestions: "Questions submitted: \(self.submittedQuestions.count)", //TODO: Check self!
                             buttonType: buttonType,
                             answeredQuestion: answeredQuestion,
