869731b17b7fd11c763f7366de21ed0c76c2ed18
Menesidis <menesidis@gmail.com>
2020-08-16 16:55:17 +0300

    Add UI tests

--- Survey/SurveyUITests/SurveyUITests.swift
+++ Survey/SurveyUITests/SurveyUITests.swift
@@ -9,35 +9,82 @@
 import XCTest
 
 class SurveyUITests: XCTestCase {
+    var app: XCUIApplication!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    // MARK: - XCTestCase
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+    override func setUp() {
+        super.setUp()
+
+        // Since UI tests are more expensive to run, it's usually a good idea
+        // to exit if a failure was encountered
         continueAfterFailure = false
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        app = XCUIApplication()
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+    func testNextBarButtonsExists() {
+        XCTAssertFalse(app.navigationBars.buttons["nextButton"].exists)
+        app.buttons["surveyButton"].tap()
+        XCTAssertTrue(app.navigationBars.buttons["nextButton"].exists)
+    }
+    
+    func testPreviousButtonEnabled() {
+        app.buttons["surveyButton"].tap()
+        XCTAssertFalse(app.navigationBars.buttons["previousButton"].isEnabled)
+        // TODO: Find a better way to wait async API call
+        // Waiting to load questions from API
+        sleep(4)
+        
+        app.navigationBars.buttons["nextButton"].tap()
+        XCTAssertTrue(app.navigationBars.buttons["previousButton"].isEnabled)
+        
+        app.navigationBars.buttons["previousButton"].tap()
+        XCTAssertFalse(app.navigationBars.buttons["previousButton"].isEnabled)
+        
+    }
+    
+    func testSubmitButtonEnabled() {
+        app.buttons["surveyButton"].tap()
+        XCTAssertFalse(app.buttons["submitButton"].isEnabled)
+        
+        let answerTextField = app.textFields["answerTextField"]
+        answerTextField.tap()
+        answerTextField.typeText("This is my answer!")
+        XCTAssertTrue(app.buttons["submitButton"].isEnabled)
+    }
+    
+    func testNavigationBannerExists() {
+        app.buttons["surveyButton"].tap()
+        
+        let answerTextField = app.textFields["answerTextField"]
+        answerTextField.tap()
+        answerTextField.typeText("This is my answer!")
+        
+        app.buttons["submitButton"].tap()
+        
+        // Waiting
+        let notificationView = XCUIApplication().otherElements["notificationView"]
+        XCTAssertFalse(notificationView.exists)
+        XCTAssertTrue(notificationView.waitForExistence(timeout: 2.5))
+    }
+    
+    func testKeyboardIsHidden() {
+        app.buttons["surveyButton"].tap()
+        
+        // Keyboard is NOT visible
+        XCTAssertFalse(app.keyboards.count > 0)
+        
+        let answerTextField = app.textFields["answerTextField"]
+        answerTextField.tap()
+        answerTextField.typeText("I will tap Return!!!")
+        
+        // Keyboard is visible
+        XCTAssertTrue(app.keyboards.count > 0)
+        XCUIApplication().keyboards.buttons["return"].tap()
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
+        // Keyboard is NOT visible
+        XCTAssertFalse(app.keyboards.count > 0)
     }
 }
